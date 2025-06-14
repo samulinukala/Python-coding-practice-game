3034cd14668f709d60874872a7599544ba9a723a
samulinukala <71631113+samulinukala@users.noreply.github.com>
2025-06-14 17:41:38 +0300

    colisions

--- python peli/python_peli.py
+++ python peli/python_peli.py
@@ -1,5 +1,12 @@
 
+from asyncio.windows_events import NULL
+from itertools import count
+from re import X
+from subprocess import ABOVE_NORMAL_PRIORITY_CLASS
 import os, sys, pygame
+from tkinter import Y
+from pygame.rect import Rect
+
 pygame.init()
 screen_width=1000
 screen_height= 600
@@ -8,44 +15,68 @@ player_height=20
 player_x=screen_width//2-player_width//2
 player_y= screen_height-player_height-10
 player_speed=2
-player_jumpdecay=0.2
-player_jumpstrength=6
+player_jumpdecay=0.3
+player_jumpstrength=8
 player_jumpPower=0
-gravity_multiplier=4
+gravity_multiplier=5
 screen=pygame.display.set_mode((screen_width,screen_height))
 pygame.display.set_caption("python testi")
 drawables=[]
+
 
 def make_platform (pos_x,pos_y,platform_width,platform_height):
     drawables.append(pygame.Rect(pos_x,pos_y,platform_width,platform_height))
 
 clock= pygame.time.Clock()
 i=0;
+
 make_platform(0,400,400,40)
 def check_col(rect1,rect2):
-    return rect1.colliderect(rect2)
+    return pygame.Rect(rect1).colliderect(pygame.Rect(rect2))
+def check_colAndRep(checkAgainst) :
+     t=0;
+     while (t<len(drawables)) :
+        if(check_col(drawables[t],checkAgainst)):
+            return drawables[t]
+        t+=1
+        
+   
+
 while True:
+    playerRect=pygame.Rect(player_x,player_y,player_width,player_height)
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             pygame.quit()
             sys.exit()
     keys=pygame.key.get_pressed()
+
     if keys[pygame.K_LEFT]and player_x>0:
         player_x-=player_speed
     if keys[pygame.K_RIGHT]and player_x <screen_width- player_width:
         player_x+= player_speed
     if keys[pygame.K_UP] and player_y>0 and player_jumpPower<=0:
         player_jumpPower=player_jumpstrength
-
-    if player_jumpPower>0 :
-         player_y-=player_jumpPower
+    dir1=pygame.Vector2(0,0)
+    collider=check_colAndRep(playerRect)
+    if collider !=NULL :
+        vec1=pygame.Vector2(player_x,player_y)
+        cord=collider as Rect.X,collider as Rect.Y
+        vec2=pygame.Vector2()
+    if  player_jumpPower>0 :
+        player_y-=player_jumpPower
+    elif (check_col(drawables[0],playerRect)) :
+        print("col")
+       # player_y-=player_height/3
+  
     else: 
         if player_y<screen_height-player_height:
             player_y+=gravity_multiplier
 
     player_jumpPower-= player_jumpdecay
     screen.fill((163,154,164))
-    check_col()
+   
+   
+    
     while (len( drawables)>i) :
     
         pygame.draw.rect(screen,(100,30,30),drawables[i])
@@ -53,7 +84,7 @@ while True:
 
     i=0;
    
-    pygame.draw.rect(screen,(0,128,255),(player_x,player_y,player_width,player_height))
+    pygame.draw.rect(screen,(0,128,255),playerRect);
     pygame.display.flip()
     clock.tick(90)
 
